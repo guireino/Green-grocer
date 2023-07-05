d3ae2885ed63ee78609fdf6fcca302a129860e35
guilherme <guiabucarma@gmail.com>
2023-07-05 18:10:58 -0400

    add os arquivos e updates do projeto 05/07/23

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,11 +1,55 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:json_annotation/json_annotation.dart';
+
+//build_runner foi usado para compilar class e para criar arquivo user_model.g.dart
+//utilizando no terminal comando: flutter pub run build_runner build para criar arquivo user_model.g.dart mesmo com aviso erro
+part 'user_model.g.dart';
+
+//utilizando plugins: dart data class generator
+@JsonSerializable()
 class UserModel {
-  String name, email, phone, cpf, password;
+  // na hora conversao ele vai mudar name para fullname
+  @JsonKey(name: 'fullname')
+  String? name;
+  String? email, phone, cpf, password, id, token;
 
   UserModel({
-    required this.name,
-    required this.email,
-    required this.phone,
-    required this.cpf,
-    required this.password,
+    this.name,
+    this.email,
+    this.phone,
+    this.cpf,
+    this.password,
+    this.id,
+    this.token,
   });
+
+  /*
+
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      token: String? name, email, phone, cpf, password, id,.fromMap(map['token'] as Map<String,dynamic>),
+    );
+  }
+
+  //trasformando objeto em um map com seus atributos
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'token': token.toMap(),
+    };
+  }
+
+  */
+
+  /// Connect the generated [_$PersonFromJson] function to the `fromJson`
+  /// factory.
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  /// Connect the generated [_$UserModelToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+
+  //returno com os valores
+  @override
+  String toString() => 'UserModel(name: $name, token: $token)';
 }
--- lib/src/pages/auth/respository/auth_respository.dart
+++ lib/src/pages/auth/respository/auth_respository.dart
@@ -1,6 +1,8 @@
 import 'package:greengrocer/src/constants/endpoints.dart';
 import 'package:greengrocer/src/services/http_manager.dart';
 
+import '../../../models/user_model.dart';
+
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
@@ -21,6 +23,11 @@ class AuthRepository {
     //result key e variavel html
     if (result['result'] != null) {
       print("Signin funcionou!");
+      //print(result['result']);
+
+      //instanciando novo usuario
+      final user = UserModel.fromJson(result['result']);
+      print(user);
     } else {
       print("Signin nao funcionou!");
       print(result['error']);
